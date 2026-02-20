dadfd7f2fad7012aabf860d9994b4ec7b1b67e2f
sangramthakur101204 <tsangram014@gmail.com>
2026-02-20 11:00:01 +0530

    Implemented menu-based student management with add and display features

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,53 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Student Id");
-        int id = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Enter Student Name");
-        String name = sc.nextLine();
-        System.out.println("Enter Student Marks");
-        double marks = sc.nextDouble();
-        Student student = new Student(id,name,marks);
-
         ArrayList<Student>students = new ArrayList<>();
-        students.add(student);
+        int choice;
 
-       for (Student s : students){
-          s.display();
-       }
+        while (true){
+            System.out.println("1. Add Student");
+            System.out.println("2. Display Students");
+            System.out.println("3. Exit");
+            System.out.println("Enter Your choice..");
+            choice = sc.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Enter Student Id");
+                    int id = sc.nextInt(); sc.nextLine();
+                    System.out.println("Enter Student Name");
+                    String name = sc.nextLine();
+                    System.out.println("Enter Student Marks");
+                    double marks = sc.nextDouble();
+                    Student student = new Student(id,name,marks);
+                    students.add(student);
+                    break;
+                case 2:
+                    for (Student s : students){
+                        s.display();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting for the program...");
+                    return;
+
+                default:
+                    System.out.println("Invalid choice...");
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
 
 
 
